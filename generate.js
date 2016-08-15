3ab0974f4fc062c49e4a8b97fc59b5ee6830335b
Tim Seckinger <seckinger.tim@gmail.com>
2016-08-15 19:35:35 +0200

    allow viz.js reuse

--- generate.js
+++ generate.js
@@ -1,6 +1,9 @@
 module.exports = function (data, args) {
     'use strict';
 
+    // viz.js function cannot be used more than once
+    delete require.cache[require.resolve('viz.js')];
+
     return require('viz.js')(data, {
         engine: args.K || args.prog,
         format: args.T || 'svg'
