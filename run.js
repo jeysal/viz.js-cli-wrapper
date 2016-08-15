570ea14a2ca697df58bdecb3ecadc0070918a50a
Tim Seckinger <seckinger.tim@gmail.com>
2016-08-15 19:00:58 +0200

    implement input file handling

--- run.js
+++ run.js
@@ -3,4 +3,16 @@ module.exports = function (args) {
 
     if (args.V)
         console.log(args.prog + ' - graphviz version 2.38.0 (20140413.2041)');
+
+    var generate = require('./generate.js');
+    var write = require('./write.js');
+
+    var fs = require('fs');
+    var target = args.o ? fs.createWriteStream(args.o) : process.stdout;
+
+    if (args.files.length)
+        args.files.forEach(function (file) {
+            var input = fs.readFileSync(file, 'utf8');
+            write(generate(input, args), target);
+        });
 };
