013d263cc36f235550b54d0c31a210ef71184a79
Tim Seckinger <seckinger.tim@gmail.com>
2016-08-19 12:47:42 +0200

    add exit and mock logic to exit.js

--- src/main/js/exit.js
+++ src/main/js/exit.js
@@ -1,4 +1,13 @@
-module.exports = {
+function exit(code) {
+    exit.fake ? exit.records.push(code) : process.exit(code);
+}
+
+exit.fake = false;
+exit.records = [];
+
+exit.codes = {
     readErr: 70,
     writeErr: 80
 };
+
+module.exports = exit;
--- src/main/js/write.js
+++ src/main/js/write.js
@@ -8,7 +8,7 @@ module.exports = function (data, index, args) {
             require('fs').writeFileSync(file, data, 'utf8');
         } catch (err) {
             console.error(err);
-            process.exit(require('./exit').writeErr);
+            process.exit(require('./exit').codes.writeErr);
         }
     else
         process.stdout.write(data);
